0658dd2305ef2d38f4feb851adbd3f2dfdcf2521
Harshit6263 <163866247+Harshit6263@users.noreply.github.com>
2024-03-26 13:10:54 +0530

    Add files via upload

---
+++ src/models/Issue.java
@@ -0,0 +1,80 @@
+package models;
+
+import java.util.Date;
+
+public class Issue {
+	private Integer id;
+	private Book book;
+	private Student student;
+	private Date issueDate;
+	private Integer issuedBy;
+	private Integer createdBy;
+	private Date createDate;
+	private Integer updatedBy;
+	private Date updateDate;
+	private int status;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Book getBook() {
+		return book;
+	}
+	public void setBook(Book book) {
+		this.book = book;
+	}
+	public Student getStudent() {
+		return student;
+	}
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+	public Date getIssueDate() {
+		return issueDate;
+	}
+	public void setIssueDate(Date issueDate) {
+		this.issueDate = issueDate;
+	}
+	public Integer getIssuedBy() {
+		return issuedBy;
+	}
+	public void setIssuedBy(Integer issuedBy) {
+		this.issuedBy = issuedBy;
+	}
+	public Integer getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(Integer createdBy) {
+		this.createdBy = createdBy;
+	}
+	public Date getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	public Integer getUpdatedBy() {
+		return updatedBy;
+	}
+	public void setUpdatedBy(Integer updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	
+}
+	
+	
+	
